0107d4a917be0e0b4390bfd60d97ee9503663bd0
Alexander Sklar <asklar@microsoft.com>
2021-09-30 13:14:56 -0700

    externalize rnx project dependencies

--- buildCustomProjection.cmd
+++ buildCustomProjection.cmd
@@ -1,15 +1,19 @@
 @echo off
 
+nuget restore windows\rn65.sln
+
 echo Building the native custom control
-msbuild .\windows\RuntimeComponent1\RuntimeComponent1.vcxproj /p:Platform=x86 
+msbuild .\windows\RuntimeComponent1\RuntimeComponent1.vcxproj /p:Platform=x86 /restore /p:RestorePackagesConfig=true
 
 echo Building the projection
 cd /d %~dp0\node_modules\react-native-xaml
 dotnet run --project .\Codegen\ -winmd ..\..\windows\RuntimeComponent1\Debug\Merged\RuntimeComponent1.winmd
-yarn add @types/react @types/react-native --save
-yarn
-npx typescript -b
+call yarn add @types/react @types/react-native --save
+call yarn
+call npx typescript -b
+
+echo Need to add a reference from ReactNativeXaml project to the RuntimeComponent1 project
 
 echo Building the app
 cd /d %~dp0
-npx react-native run-windows --logging
+call npx react-native run-windows --logging
